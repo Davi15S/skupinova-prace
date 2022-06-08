a3e560373da085794a9d2d66be18188a468ea43f
TwenntyOnee <kristian.ulrych@gmail.com>
2022-06-08 17:10:02 +0200

    News item add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Link from "next/link";
+import NewsItem from "../components/NewsItem";
 import {
   useGetCategoriesQuery,
   useGetPostsQuery,
@@ -20,64 +21,11 @@ const Home: NextPage = () => {
   } = useGetCategoriesQuery();
 
   return (
-    <div
-      style={{
-        margin: "0 2rem",
-      }}
-    >
-      <div>
-        <h1 className="text-2xl text-red-700">Generovaný query Page</h1>
-        <Link href={"/manual-gql"}>
-          <a>
-            <button>Manuální query a types</button>
-          </a>
-        </Link>
-      </div>
-      <div className="">
-        <h1>
-          <b>Kategorie</b>
-        </h1>
-        {loadingCategories ? (
-          <p>Načítání</p>
-        ) : errorCategories ? (
-          <p>Error</p>
-        ) : (
-          <div>
-            {dataCategories?.categories?.nodes?.map((item) => (
-              <div key={item?.id}>
-                <p>Name: {item?.name}</p>
-                <p>Slug: {item?.slug}</p>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-      {loading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p>An error has occurred.</p>
-      ) : !data?.posts?.edges ? (
-        <p>No posts found.</p>
-      ) : (
-        data?.posts?.edges.map((post) => {
-          return (
-            <article
-              key={post?.node?.id}
-              style={{
-                border: "2px solid #eee",
-                padding: "1rem",
-                marginBottom: "1rem",
-                borderRadius: "10px",
-              }}
-            >
-              <h2>{post?.node?.title}</h2>
-              <p>Slug: {post?.node?.slug}</p>
-              <p>Autor id: {post?.node?.authorId}</p>
-            </article>
-          );
-        })
-      )}
-    </div>
+    <NewsItem
+      title="Březnové vydání fotbalového magazínu Offside"
+      image="https://play-lh.googleusercontent.com/cvcQMYwlebh23KkUjShTZ4nzq1ubapxjOAjA5cdL41kdfOWKjfR7kgp0DOilfAnxTa8"
+      content="V únorovém vydání fotbalového magazínu Offside můžete zjistit více například o skvělé formě Antonína Baráka, o nejzajímavějších přestupech ve Fortuna Lize nebo o problémech Belgického fotbalisty Lukaka. Toto vše a mnohem více z fotbalového dění u nás i ve světě se dozvíte v tomto novém vydání!"
+    ></NewsItem>
   );
 };
 
