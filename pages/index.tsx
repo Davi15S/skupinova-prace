ff09e9c032856cfb0bb8509e5a961304a04248c4
Davi15S <nguyenxd072@gmail.com>
2022-06-11 15:11:57 +0200

    main page done

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,23 +17,35 @@ const Home: NextPage = () => {
   const { data, loading, error } = queryResponse;
 
   return (
-    <div className="mainscreen">
-      <div className="image w-full justify-center items-center flex relative h-[589px]">
-        <Image
-          className=" object-cover "
-          layout="fill"
-          src="https://images.pexels.com/photos/1269805/pexels-photo-1269805.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        ></Image>
-        <div className="darker w-full h-[589px] bg-[#00000066] absolute"></div>
-        <h1 className="absolute font-bold text-white md:text-5xl lg:text-9xl">
-          NOVINKY Z PRAHY
-        </h1>
-      </div>
-      <NewsItem
-        title="Březnové vydání fotbalového magazínu Offside"
-        image="https://play-lh.googleusercontent.com/cvcQMYwlebh23KkUjShTZ4nzq1ubapxjOAjA5cdL41kdfOWKjfR7kgp0DOilfAnxTa8"
-        content="V únorovém vydání fotbalového magazínu Offside můžete zjistit více například o skvělé formě Antonína Baráka, o nejzajímavějších přestupech ve Fortuna Lize nebo o problémech Belgického fotbalisty Lukaka. Toto vše a mnohem více z fotbalového dění u nás i ve světě se dozvíte v tomto novém vydání!"
-      ></NewsItem>
+    <div>
+      {loading ? (
+        <div></div>
+      ) : (
+        <div className="mainscreen">
+          <div className="image w-full justify-center items-center flex relative h-[589px]">
+            <Image
+              className=" object-cover "
+              layout="fill"
+              src="https://images.pexels.com/photos/1269805/pexels-photo-1269805.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            ></Image>
+            <div className="darker w-full h-[589px] bg-[#00000066] absolute"></div>
+            <h1 className="absolute font-bold text-white md:text-5xl lg:text-9xl">
+              NOVINKY Z PRAHY
+            </h1>
+          </div>
+          <div className="flex flex-wrap justify-center">
+            {data?.posts?.nodes?.map((item) => (
+                <NewsItem
+                  key={item?.id}
+                  title={item?.title!}
+                  image={item?.featuredImage?.node?.sourceUrl!}
+                  content={item?.content!}
+                  link={item?.slug!}
+                ></NewsItem>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
